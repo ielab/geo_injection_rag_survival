34aa4bdf99db88392950b3504a27a4f417472d98
YinYuBB <yinyu201906@gmail.com>
2026-05-14 14:29:31 +1000

    update

--- RAG_attack_pipeline/llm-rankers/setup.py
+++ RAG_attack_pipeline/llm-rankers/setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='llm-rankers',
     version='0.0.2',
@@ -13,8 +10,6 @@ setup(
     author_email='s.zhuang@uq.edu.au',
     description='Pointwise, Listwise, Pairwise and Setwise Document Ranking with Large Language Models.',
     python_requires='>=3.8',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     install_requires=[
         "transformers>=4.31.0",
     ]
